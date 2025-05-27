d1c750a72c2b4786d6af0bca877a150bb72292cd
Srikeerthi05 <srikeerthibavanila@gmail.com>
2025-05-27 16:05:21 +0530

    Create automated tests for order summary

--- .history/scripts/checkout/orderSummary_20250525200357.js
+++ .history/scripts/checkout/orderSummary_20250525200357.js
@@ -35,7 +35,7 @@ export function renderOrderSummary(){
 
   
   cartSummaryHTML+=   `
-      <div class="cart-item-container js-cart-item-container js-cart-item-container-${matchingProduct.id}">
+      <div class="cart-item-container js-cart-item-cpon js-cart-item-container-${matchingProduct.id}">
               <div class="delivery-date">
                 Delivery date: ${dateString}
               </div>
@@ -51,14 +51,14 @@ export function renderOrderSummary(){
                   <div class="product-price">
                     $${formatCurrency(matchingProduct.priceCents)}
                   </div>
-                  <div class="product-quantity js-product-quantity-${matchingProduct.id}">
+                  <div class="product-quantity">
                     <span>
                       Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                     </span>
                     <span class="update-quantity-link link-primary">
                       Update
                     </span>
-                    <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
+                    <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                       Delete
                     </span>
                   </div>
--- .history/scripts/checkout/orderSummary_20250525201406.js
+++ .history/scripts/checkout/orderSummary_20250525201406.js
@@ -51,14 +51,14 @@ export function renderOrderSummary(){
                   <div class="product-price">
                     $${formatCurrency(matchingProduct.priceCents)}
                   </div>
-                  <div class="product-quantity js-product-quantity-${matchingProduct.id}">
+                  <div class="product-quantity ">
                     <span>
                       Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                     </span>
                     <span class="update-quantity-link link-primary">
                       Update
                     </span>
-                    <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
+                    <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                       Delete
                     </span>
                   </div>
